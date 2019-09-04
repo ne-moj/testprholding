5cbff1ae48f527ed1e815bf581de5391746b0138
Sergey Krasovsky <sergey.krasowski@gmail.com>
2019-09-04 12:38:15 +0300

    [master] ks. Add sql code for create Tables

---
+++ sqlinstall/apples.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `apples` (
+    `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT ,
+    `tree_id` INT(11) UNSIGNED NOT NULL DEFAULT '0' ,
+    `color` VARCHAR(7) NOT NULL DEFAULT '#00B454' ,
+    `eaten` TINYINT UNSIGNED NOT NULL DEFAULT '0' ,
+    `status` ENUM('handing','lay','decayed','') NOT NULL DEFAULT 'handing' ,
+    `pos_x` SMALLINT NOT NULL DEFAULT '0' ,
+    `pos_y` SMALLINT NOT NULL DEFAULT '0' ,
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    `fell_in` DATETIME,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
